ea174dbb113203c89bf00f550f94f1e8f1c0a999
Frank-Rj <87914653+Frank-Rj@users.noreply.github.com>
2021-11-08 19:33:18 -0600

    Subido

---
+++ CajeroAutomatico/CajeroAutomatico/Controllers/CajeroController.cs
@@ -0,0 +1,45 @@
+﻿using CajeroAutomatico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroAutomatico.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult index(Cajero c)
+        {
+            _ = c;
+
+            if(c.Monto % 5 == 0)
+            {
+                return View("PaginaCorrecta");
+            }
+            else
+            {
+                return View("PaginaError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult PaginaError()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult PaginaCorrecta()
+        {
+            return View();
+        }
+    }
+}
